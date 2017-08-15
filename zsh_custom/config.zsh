adadd9e6d6e5ad4f4efd1d4a832739d57ff4e731
Myrice <qqlddg@gmail.com>
2017-08-14 19:28:31 -0700

    Change config.zsh tab indent to 4 spaces

--- zsh_custom/config.zsh
+++ zsh_custom/config.zsh
@@ -1,21 +1,21 @@
 function set_aliases() {
-	# Alias
-	alias rm='rm -i'
-	alias cp='cp -i'
-	alias mv='mv -i'
-	alias sudo='sudo '
-	alias dirs='dirs -v'
-	alias ec='emacsclient -a ""'
-	alias ect='emacsclient -t -a ""'
-	alias ecc='emacsclient -c -a ""'
-  alias ecn='emacsclient -n'
-	alias eck='emacsclient -e "(kill-emacs)"'
-  alias emacsd='emacs --daemon'
+    # Alias
+    alias rm='rm -i'
+    alias cp='cp -i'
+    alias mv='mv -i'
+    alias sudo='sudo '
+    alias dirs='dirs -v'
+    alias ec='emacsclient -a ""'
+    alias ect='emacsclient -t -a ""'
+    alias ecc='emacsclient -c -a ""'
+    alias ecn='emacsclient -n'
+    alias eck='emacsclient -e "(kill-emacs)"'
+    alias emacsd='emacs --daemon'
 }
 
 function set_perforce() {
-	export P4ENVIRO="$HOME/.p4config"
-	export P4CONFIG=".p4config"
+    export P4ENVIRO="$HOME/.p4config"
+    export P4CONFIG=".p4config"
 }
 
 function set_dirs() {
