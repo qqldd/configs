f45d33401914f29eea0a7113d572f808617aa1c6
myrice <qqlddg@gmail.com>
2018-01-25 15:21:06 -0800

    [zsh] Change zsh_custom script indentation to 2 space

--- zsh_custom/config.zsh
+++ zsh_custom/config.zsh
@@ -1,21 +1,27 @@
 function set_aliases() {
-    # Alias
-    alias rm='rm -i'
-    alias cp='cp -i'
-    alias mv='mv -i'
-    alias sudo='sudo '
-    alias dirs='dirs -v'
-    alias ec='emacsclient -a ""'
-    alias ect='emacsclient -t -a ""'
-    alias ecc='emacsclient -n -c -a ""'
-    alias ecn='emacsclient -n'
-    alias eck='emacsclient -e "(kill-emacs)"'
-    alias emacsd='emacs --daemon'
+  # Alias
+  alias rm='rm -i'
+  alias cp='cp -i'
+  alias mv='mv -i'
+  alias sudo='sudo '
+  alias dirs='dirs -v'
+  alias ec='emacsclient -a ""'
+  alias ect='emacsclient -t -a ""'
+  alias ecn='emacsclient -n'
+  alias ecc='emacsclient -c -n -a ""'
+  alias eck='emacsclient -e "(kill-emacs)"'
+  alias emacsd='emacs --daemon'
+  alias diff='colordiff'
+  alias open='xdg-open'
+}
+
 }
 
 function set_perforce() {
-    export P4ENVIRO="$HOME/.p4config"
-    export P4CONFIG=".p4config"
+  export P4ENVIRO="$HOME/.p4config"
+  export P4CONFIG=".p4config"
+  export P4DIFF='p4merge'
+  export P4EDITOR='emacsclient -a ""'
 }
 
 function set_dirs() {
