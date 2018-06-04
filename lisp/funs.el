277c282ee3fb088db7557fcf5985f8980b836d16
myrice <qqlddg@gmail.com>
2018-06-04 16:15:10 -0700

    [EMACS] Add lisp functions.

---
+++ lisp/funs.el
@@ -0,0 +1,11 @@
+
+(defun set-font12 ()
+  (interactive)
+  (set-frame-font "-PfEd-DejaVuSansMono Nerd Font-normal-normal-normal-*-12-*-*-*-*-0-iso10646-1" nil t))
+
+
+(defun set-font14 ()
+  (interactive)
+  (set-frame-font "-PfEd-DejaVuSansMono Nerd Font-normal-normal-normal-*-14-*-*-*-*-0-iso10646-1" nil t))
+
+(provide 'funs)
