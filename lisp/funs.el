b4cf3c1838967c1e9576e3d2f63431e6518d2dff
myrice <qqlddg@gmail.com>
2018-06-22 11:08:35 -0700

    [Emacs] Add function to revert all buffers.

--- lisp/funs.el
+++ lisp/funs.el
@@ -8,4 +8,14 @@
   (interactive)
   (set-frame-font "-PfEd-DejaVuSansMono Nerd Font-normal-normal-normal-*-14-*-*-*-*-0-iso10646-1" nil t))
 
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (when (yes-or-no-p "Are you sure - any changes in open buffers will be lost! ")
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files.") ))
+
 (provide 'funs)
