93a76903e079e75f10bc8c93f5e80fe1cb46f5e9
myrice <qqlddg@gmail.com>
2018-08-24 15:19:49 -0700

    [Zsh] Added function to print 256 colors.

--- zsh_custom/funs.zsh
+++ zsh_custom/funs.zsh
@@ -18,3 +18,12 @@ function tmux-sendall() {
 fh() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
+
+function print256Color() {
+  for i in {0..255} ; do
+    printf "\x1b[38;5;${i}m%3d " "${i}"
+    if (( $i == 15 )) || (( $i > 15 )) && (( ($i-15) % 12 == 0 )); then
+      echo;
+    fi
+  done
+}
