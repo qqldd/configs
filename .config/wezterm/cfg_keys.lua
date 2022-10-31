cced5050004eff2caf4f28f0c5ee096618db25ef
Myrice <qqlddg@gmail.com>
2022-10-31 11:58:55 -0700

    [WezTerm] Remove Fn switch tab

--- .config/wezterm/cfg_keys.lua
+++ .config/wezterm/cfg_keys.lua
@@ -49,15 +49,10 @@ local mykeys = {
   },
 }
 for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
+  -- LEADER + number to activate that tab
   table.insert(mykeys, {
     key = tostring(i),
     mods = 'LEADER',
-    action = act.ActivateTab(i - 1),
-  })
-  -- F1 through F8 to activate that tab
-  table.insert(mykeys, {
-    key = 'F' .. tostring(i),
     action = act.ActivateTab(i - 1),
   })
 end
